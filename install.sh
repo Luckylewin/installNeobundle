5ac22653fab94c1a93b97ae7ce19ac3ca7e50e6a
Luckylewin <876505905@qq.com>
2017-04-18 09:44:44 +0800

    Create install.sh
    this is the shell of install NeoBundle,you should execute ":NeoBundleUpdate" in the vim after execute this shell to install the plugin.

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash                                                                                                                                                                                                                                                                   
+home=~
+vimrc=$home'/.vimrc'
+path=$home'/.vim/bundle'
+config=$path'/vim-airline/doc/airline.txt'
+cd ~
+if [ -e $vimrc ]
+then
+        isInstall=$(cat $vimrc|grep neobundle|wc -l)
+
+      if [[ -d $path || $isInstall -ne 0 ]]
+        then
+            echo "Already install NeoBundle,please rm -rf the dir '~/.vim/bundle' and remove the config about 'neobundle' from file of .vimrc"  && exit 0
+      fi
+else
+        touch $vimrc;
+fi
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+    echo " 
+         set t_Co=256
+         set laststatus=2
+         set cursorline
+         set number
+         if 0 | endif
+         if &compatible
+         endif
+         set runtimepath+=~/.vim/bundle/neobundle.vim/
+         call neobundle#begin(expand('~/.vim/bundle/'))" >> $vimrc
+
+         echo " NeoBundleFetch 'Shougo/neobundle.vim'" >> $vimrc
+         echo " NeoBundle      'vim-airline/vim-airline.git'" >> $vimrc
+         echo " NeoBundle      'chr4/nginx.vim'" >> $vimrc
+         echo " NeoBundle      'scrooloose/nerdtree.git'" >> $vimrc
+         echo "call neobundle#end()
+               filetype plugin indent on
+               NeoBundleCheck" >> $vimrc
+
+        echo '"---------------airline options------------------------' >> $vimrc
+        echo 'let g:airline#extensions#tabline#enabled=1 "顶部tab显示"' >> $vimrc
+        sed -n '193,195p' $config >> ~/.vimrc
+        sed -n '216,223p' $config >> ~/.vimrc
+        echo '"---------------nmap options-------------------------------------' >> $vimrc
+        echo ' nmap <F5> :NERDTree<cr>
+               nmap <tab> :bn<cr>
+               nmap <F6> :q!<cr>' >> $vimrc
